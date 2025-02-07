0cc6c0acb5b1c0ba5c2ca654c0b0bb3e1c5c4405
Minhaj Munna <lnushaikreact@gmail.com>
2025-02-06 19:05:57 -0600

    model changes

---
+++ BulkyWeb/Models/Categoy.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace BulkyWeb.Models
+{
+    public class Categoy
+    {
+        [Key] //primary key
+        public int Id { get; set; }
+        [Required] //not null setting
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
